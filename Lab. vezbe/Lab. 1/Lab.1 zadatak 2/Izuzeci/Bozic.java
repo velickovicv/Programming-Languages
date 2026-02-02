8beee7058ff1e64a3f6ddd22e53029b849082ef0
velickovicv <160411101+velickovicv@users.noreply.github.com>
2026-02-02 16:23:08 +0100

    Add Bozic exception class

---
+++ Lab. vezbe/Lab. 1/Lab.1 zadatak 2/Izuzeci/Bozic.java
@@ -0,0 +1,18 @@
+package Izuzeci;
+
+public class Bozic extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	public Bozic(String s) 
+	// Arguments je String, jer zelimo 
+	
+	//da posalje poruku o izuzetku
+	
+	{
+		super(s);
+	}
+	
+}
